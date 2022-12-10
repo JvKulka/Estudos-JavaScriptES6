a0bbf4005d35576a75a197cbe4067c4d8e05713e
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-09 21:01:25 -0300

    Realizado exercícios sobre Strings | Finalizado conteúdo de Strings

--- string.js
+++ string.js
@@ -114,3 +114,79 @@ const valor = '  R$ 23.00   '
 valor.trim(); // 'R$ 23.00'
 valor.trimStart(); // 'R$ 23.00   '
 valor.trimEnd(); // '  R$ 23.00'
+
+
+
+/* ----------------------------------------------- EXERCICIOS --------------------------------------------------------*/
+
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+    {
+        descricao: 'Taxa do Pão',
+        valor: 'R$ 39',
+    },
+    {
+        descricao: 'Taxa do Mercado',
+        valor: 'R$ 129',
+    },
+    {
+        descricao: 'Recebimento de Cliente',
+        valor: 'R$ 99',
+    },
+    {
+        descricao: 'Taxa do Banco',
+        valor: 'R$ 129',
+    },
+    {
+        descricao: 'Recebimento de Cliente',
+        valor: 'R$ 49',
+    },
+];
+
+let taxaTotal = 0
+let recebimentoTotal = 0
+transacoes.forEach((item) => {
+    const numeroLimpo = +item.valor.replace('R$ ', '')
+    if (item.descricao.slice(0, 4) === 'Taxa') {
+        taxaTotal += numeroLimpo
+    } else {
+        recebimentoTotal += numeroLimpo
+    }
+})
+
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const arrayTransportes = transportes.split(';')
+console.log(arrayItens)
+
+// Substitua todos os span's por a's
+let html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
+
+html = html.replaceAll('span', 'a');
+console.log(html)
+
+// Retorne o último caracter da frase
+const frase4 = 'Melhor do ano!';
+
+console.log(frase4[frase4.length - 1])
+
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+let totalTaxas = 0
+transacoes2.forEach((item) => {
+    item = item.toLowerCase()
+    item = item.trim()
+    item = item.slice(0, 4)
+    if (item === 'taxa')
+        totalTaxas++
+})
+
+console.log(totalTaxas)
