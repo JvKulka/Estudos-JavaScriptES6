ead236bc1d36ca8bebc7fa00f794b22b54433574
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-20 23:57:44 -0300

    Dado continuidade nos estudos de function

--- function.js
+++ function.js
@@ -62,3 +62,94 @@ Dom.prototype.ativo = function (classe) {
 const lista = new Dom('ul');
 lista.ativo('ativar');
 console.log(lista);
+
+//O novo valor de this deve ser semelhante a estrutura do valor do this original do método. 
+//Caso contrário o método não conseguirá interagir de forma correta com o novo this.
+const novoSeletor = {
+    element: document.querySelector('li')
+}
+
+Dom.prototype.ativo.call(novoSeletor, 'ativar');
+
+
+//É comum utilizarmos o call() nas funções do protótipo do construtor Array.
+//Assim podemos estender todos os métodos de Array à objetos que se parecem com uma Array (array-like).
+Array.prototype.mostreThis = function () {
+    console.log(this);
+}
+
+const frutas2 = ['Uva', 'Maçã', 'Banana'];
+frutas.mostreThis(); // ['Uva', 'Maçã', 'Banana']
+
+Array.prototype.pop.call(frutas2); // Remove Banana
+frutas.pop(); // Mesma coisa que a função acima
+
+//HTMLCollection, NodeList e demais objetos do Dom, são parecidos com uma array. 
+//Por isso conseguimos utilizar os mesmos na substituição do this em call.
+const li = document.querySelectorAll('li');
+
+const filtro = Array.prototype.filter.call(li, function (item) {
+    return item.classList.contains('ativo');
+});
+
+filtro; // Retorna os itens que possuem ativo
+
+//O apply(this, [arg1, arg2, ...]) funciona como o call, a única diferença é que os argumentos da função são passados através de uma array.
+const numeros = [3, 4, 6, 1, 34, 44, 32];
+Math.max.apply(null, numeros);
+Math.max.call(null, 3, 4, 5, 6, 7, 20);
+
+// Podemos passar null para o valor
+// de this, caso a função não utilize
+// o objeto principal para funcionar
+
+
+//A única diferença é a array como segundo argumento.
+const li2 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item) {
+    return item.classList.contains('ativo');
+}
+
+const filtro1 = Array.prototype.filter.call(li2, itemPossuiAtivo);
+const filtro2 = Array.prototype.filter.apply(li2, [itemPossuiAtivo]);
+
+
+//Diferente de call e apply, bind(this, arg1, arg2, ...) não irá executar a função mas sim retornar a mesma com o novo contexto de this.
+const li3 = document.querySelectorAll('li');
+
+const filtrarLi = Array.prototype.filter.bind(li3, function (item) {
+    return item.classList.contains('ativo');
+});
+
+filtrarLi();
+
+
+//Não precisamos passar todos os argumentos no momento do bind, podemos passar os mesmos na nova função no momento da execução da mesma.
+const carro2 = {
+    marca: 'Ford',
+    ano: 2018,
+    acelerar: function (aceleracao, tempo) {
+        return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
+    }
+}
+carro2.acelerar(100, 20);
+// Ford acelerou 100 em 20
+
+const honda = {
+    marca: 'Honda',
+};
+const acelerarHonda = carro.acelerar.bind(honda);
+acelerarHonda(200, 10);
+// Honda acelerou 200 em 10
+
+
+//Podemos passar argumentos padrões para uma função e retornar uma nova função.
+function imc(altura, peso) {
+    return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80);
+
+imc(1.80, 70); // 21.6
+imc180(70); // 21.6
