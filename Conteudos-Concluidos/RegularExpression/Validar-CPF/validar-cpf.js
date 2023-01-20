55b017144b9d7b524df73d5e725ad25ca67266d8
João Victor Kulka <jv.kulka13@gmail.com>
2023-01-20 16:26:32 -0300

    Comentários Logica Validação CPF
    Relizado comentarios para explicação do código da logica de validação de CPFS.

--- Conteudos-Concluidos/RegularExpression/Validar-CPF/validar-cpf.js
+++ Conteudos-Concluidos/RegularExpression/Validar-CPF/validar-cpf.js
@@ -8,20 +8,24 @@ export default class ValidarCPF {
         return cpf.replace(/\D/g, '')
     }
 
+    //função para construir padrão do cpf com regexp
     construir(cpf) {
         return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4')
     }
 
+    //função para limpar e construir padrão do cpf
     formatar(cpf) {
         const cpfLimpo = this.limpar(cpf)
         return this.construir(cpfLimpo)
     }
 
+    //Função que verifica se o CPF é valido através de Regexp
     validar(cpf) {
         const matchCPF = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g)
         return (matchCPF && matchCPF[0] === cpf)
     }
 
+    //Função que verifica se ocorreu a mudança e se o CPF está no padrão correto
     validarNaMudança(cpfElement) {
         if (this.validar(cpfElement.value)) {
             cpfElement.value = this.formatar(cpfElement.value)
@@ -35,12 +39,14 @@ export default class ValidarCPF {
         }
     }
 
+    //Função que adiciona o evento de mudança após o usuario inserir o CPF
     adicionarEvento() {
         this.element.addEventListener('change', () => {
             this.validarNaMudança(this.element)
         })
     }
 
+    //Função que adiciona um span com a mensagem de erro caso o CPF esteja incorreto
     adicionarErroSpan() {
         const erroElement = document.createElement('span')
         erroElement.classList.add('erro-text')
